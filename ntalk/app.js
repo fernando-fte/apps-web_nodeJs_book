a3cdd262e3092842d5a0781e13b4757dc88f7335
fernando-fte <fernando.fte@gmail.com>
2018-03-20 13:33:02 -0300

    Iniciando com o Express::Gerando scaffold do projeto::39-41
    Implementação do express-load

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,15 +1,18 @@
 var express =  require('express'),
-	routes = require('./routes');
+	load = require('express-load'),
+	app = express();
 
-var app = express();
+load('models')
+	.then('controllers')
+	.then('routes')
+	.into(app);
 
 app.set('views', __dirname + '/views');
 app.set('views engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', routes.index);
-app.get('/usuarios', routes.user.index);
-
+// app.get('/', routes.index);
+// app.get('/usuarios', routes.user.index);
 
 app.listen(3000, function(){
 	console.log('Ntalk no ar.');
